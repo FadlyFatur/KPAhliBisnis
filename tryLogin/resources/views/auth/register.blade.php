5876649959e25f01c7fe1b642cc428ead2f2a173
Zulfikri Mirza <zulfikrimirza@yahoo.com>
2020-07-06 14:30:14 +0700

    updaret Register form

--- tryLogin/resources/views/auth/register.blade.php
+++ tryLogin/resources/views/auth/register.blade.php
@@ -15,35 +15,78 @@
                   <form class="form-container">
                     <h1>Register</h1>
                     <div class="form-row">
-                      <div class="form-group col-md-6">
-                        <label for="inputEmail4">First name</label>
-                        <input type="text" class="form-control" id="inputEmail4">
-                      </div>
-                      <div class="form-group col-md-6">
-                        <label for="inputEmail4">Last name</label>
-                        <input type="text" class="form-control" id="inputEmail4">
-                      </div>
-                      <div class="form-group col-md-12">
-                        <label for="inputEmail4">No.HP</label>
-                        <input type="number" class="form-control" id="inputEmail4">
-                      </div>
-                      <div class="form-group col-md-12">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4">
-                      </div>
-                      <div class="form-group col-md-12">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4">
-                      </div>
-                      <div class="form-group col-md-12">
-                        <label for="inputPassword4">Confirm Password</label>
-                        <input type="password" class="form-control" id="inputPassword4">
-                      </div>
+                      <div class="form-group row">
+                              <label for="name" class="col-md-4 col-form-label ">{{ __('Name') }}</label>
+
+                              <div class="col-md-12">
+                                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                  @error('name')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                          <label for="number" class="col-md-4 col-form-label">{{ __('NO.HP') }}</label>
+                          <div class="col-md-12">
+                                  <input id="name" type="number" class="form-control @error('no.hp') is-invalid @enderror" name="no.hp" value="{{ old('no.hp') }}" required autocomplete="no.hp" autofocus>
+
+                                  @error('no.hp')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                        <div class="form-group row">
+                              <label for="email" class="col-md-4 col-form-label">{{ __('E-Mail Address') }}</label>
+
+                              <div class="col-md-12">
+                                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                  @error('email')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                            <div class="form-group row">
+                              <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
+
+                              <div class="col-md-12">
+                                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                  @error('password')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+
+                          <div class="form-group row">
+                              <label for="password-confirm" class="col-md-4 col-form-label">{{ __('Confirm Password') }}</label>
+
+                              <div class="col-md-12">
+                                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                              </div>
+                          </div>
                     </div>
                     <div class="form-group">
                     </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
-                    <a href="#">Already Have an account?</a>
+                    <div class="form-group row mb-0">
+                            <div class="col-md-6-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                                <a href="#" type="submit">
+                                    {{__('Already have an account?') }}
+                                </a>
+                            </div>
+                        </div>
                   </form>
                   <!--form end-->
                 </div>
