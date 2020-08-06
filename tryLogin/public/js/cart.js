5196879af59c2fe7acb0fad6d60e68627bab5b5c
FadlyFTR <49541723+FadlyFTR@users.noreply.github.com>
2020-08-06 11:53:54 +0700

    update cart.js funtion

--- tryLogin/public/js/cart.js
+++ tryLogin/public/js/cart.js
@@ -1,16 +1,25 @@
 console.log("running");
 
 let carts = document.querySelectorAll('.add-cart');
-// let product = [
-//   {
-//       name:
-//   }
-// ]
+let product = [
+                {
+                  name:"Foto produk P1",
+                  tag:"Paket 1",
+                  price:10000,
+                  incart:0
+                },
+                {
+                  name:"foto produk P2",
+                  tag:"Paket 2",
+                  price:250000,
+                  incart:0
+                }
+              ];
 
 for (let i = 0; i < carts.length; i++) {
   carts[i].addEventListener('click',()=>{
     // console.log("ADDED TO CART");
-    cartNumbers();
+    cartNumbers(product[i]);
   })
 }
 
@@ -23,8 +32,9 @@ function onLoadCart(){
 }
 
 
-function cartNumbers(){
+function cartNumbers(product){
   let productNum = localStorage.getItem('cartNumbers');
+
   productNum = parseInt(productNum);
 
   if(productNum){
@@ -34,6 +44,30 @@ function cartNumbers(){
     localStorage.setItem('cartNumbers', 1);
     document.querySelector('.cart span').textContent = 1;
   }
+
+  setItem(product);
+}
+
+function setItem(product){
+  let cartItems = localStorage.getItem('productinCart');
+  cartItems = JSON.parse(cartItems);
+  console.log(product);
+
+  if(cartItems != null){
+    if (cartItems[product.tag] == undefined) {
+      cartItems ={
+        ...cartItems,
+        [product.tag]: product
+      }
+    }
+    cartItems[product.tag].incart += 1;
+  }else{
+    product.incart = 1;
+    cartItems = {
+      [product.tag]: product
+    }
+  }
+  localStorage.setItem("productinCart", JSON.stringify(cartItems));
 }
 
 onLoadCart();
