8539ade8778edfe380392b790dbddecf1f3f36bf
Zulfikri Mirza <zulfikrimirza@yahoo.com>
2020-08-19 00:25:19 +0700

    verification design on progress

--- tryLogin/resources/views/auth/verify.blade.php
+++ tryLogin/resources/views/auth/verify.blade.php
@@ -1,24 +1,28 @@
 @extends('layouts.app')
-
+<link rel="stylesheet" href="css/verify.css" type="text/css">
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+        <div class="col-md-10 mt-4">
 
-                <div class="card-body">
+            <div class="card">
+                <div class="mt-3" style="width:100%; text-align:center">
+                        <img src="{{ URL::asset('img/iconLogo.webp') }}" width="50" height="50">
+                </div>
+                <b><div class="mt-4" style="text-align:center">{{ __('Verify Your Email Address') }}</div></b>
+
+                <div class="card-body text-center">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
-
+                    
                     {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('If you did not receive the email') }}
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link btn-success text-white mt-3 mb-3">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
             </div>
